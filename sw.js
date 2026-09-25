988bd7b5c7fa34471d53bb8eaa6ac8db44113697
Jan Luca Nering <jl.n@gmx.de>
2026-09-25 15:21:17 +0200

    SW loescht alte Caches, v4

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-const CACHE = "outtrack-v3";
+const CACHE = "outtrack-v4";
 const ASSETS = ["./index.html", "./styles.css", "./meals.js", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-self.addEventListener("activate", (e) => { e.waitUntil(self.clients.claim()); });
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
 self.addEventListener("fetch", (e) => {
   e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request).catch(() => caches.match("./index.html"))));
 });
