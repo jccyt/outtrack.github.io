1d4f2ef1f16151a37adc7453ce6b7e0b56dfb4ba
Jan Luca Nering <jl.n@gmx.de>
2026-09-25 15:13:19 +0200

    Untere Freiflaeche verstaerkt, Scroll-Padding, SW-Cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "outtrack-v2";
+const CACHE = "outtrack-v3";
 const ASSETS = ["./index.html", "./styles.css", "./meals.js", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
